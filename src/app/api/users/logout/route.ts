8ad72e6718bf6db15d3f462f6ad289e8b50611cf
Ashish <savitayadavmansarover@gmail.com>
2023-07-09 18:11:18 +0530

    remover NextResponse from logout route

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,13 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export function GET(response: NextResponse) {
+import { NextResponse } from "next/server";
+export async function GET() {
     try {
-        const response = NextResponse.json({ success: true, message: "Logout successfully" });
-        response.cookies.set("token", "", { httpOnly: true, expires: new Date(0) });
-        console.log("logout done ,,, i am backend");
+        const response = NextResponse.json(
+            {
+                message: "Logout successful",
+                success: true,
+            }
+        )
+        response.cookies.set("token", "",
+            {
+                httpOnly: true, expires: new Date(0)
+            });
         return response;
     } catch (error: any) {
-        console.log(error);
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
+
 }
